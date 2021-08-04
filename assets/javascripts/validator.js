2e8906e46cf6b9f4311216c388147d1b1625b6e6
dinhson <dinhthanhsondtu@gmail.com>
2021-08-04 12:26:47 +0700

    Fix comment menter Vo Dinh Duy

--- assets/javascripts/validator.js
+++ assets/javascripts/validator.js
@@ -1,11 +1,11 @@
 
 function Validator(options){
-  let selectorRules ={};
+  let selectorRules = {};
   function validate(inputElement,rule){
     let errorMessage;
     let errorElemnt = inputElement.parentElement.querySelector(options.errorselector)
     let rules = selectorRules[rule.selector];
-    for(let i =0; i<rules.length;i++){
+    for(let i = 0; i < rules.length;i++){
       errorMessage = rules[i](inputElement.value);
       if (errorMessage) break;
     }
@@ -25,7 +25,7 @@ function Validator(options){
         let isFormValue = true;
         options.rules.forEach(function (rule){
         let inputElement = formElement.querySelector(rule.selector);
-        let isValid=validate(inputElement,rule);
+        let isValid = validate(inputElement,rule);
         if(!isValid){
           isFormValue = false;
         }
@@ -37,8 +37,7 @@ function Validator(options){
             values[input.name] = input.value;
             return  values;
           }, {});
-          console.log(formValue);
-          document.getElementById('span1').innerText =formValue;
+          document.getElementById('span1').innerText = formValue;
           options.onsubmit(formValue);
         } else {
           formElement.submit();
@@ -63,24 +62,41 @@ function Validator(options){
         }
       }
     });
-    console.log(selectorRules);
   }
 }
-Validator.isRequired = function(selector,max){
+function empty(value,messageEmpty){
+  let valueTrim = value.trim();
+  if(valueTrim.length == 0){
+    return  messageEmpty;
+  }
+}
+function minLength(value,min,messageMin){
+  let valueTrim = value.trim();
+  if(valueTrim.length < min){
+    return  messageMin;
+  }
+}
+function maxLength(value,max,messageMax){
+  let valueTrim = value.trim();
+  if(valueTrim.length > max){
+    return  messageMax;
+  }
+}
+function regex(value,regex,messageRegex){
+  let valueTrim = value.trim();
+  return regex.test(value) ?  undefined :messageRegex;
+}
+let message,messageEmpty ;
+Validator.isRequired = function(selector){
   return {
     selector: selector,
     test: function(value){
       const REGEX_FULLNAME = /^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/;
-      let valueTrim = value.trim();
-      if(valueTrim.length == 0)
-      {
-        return 'Vui lòng nhập fullname';
-      }
-      if(valueTrim.length>max){
-        return `Vui lòng nhập full name ít hơn ${max} ký tự`
-      }
-      
-      return REGEX_FULLNAME.test(value) ? undefined : "Vui lòng nhập full name hợp lệ";
+      messageEmpty = "Vui lòng nhập fullname";
+      messageRegex = "Vui lòng nhập fullname hợp lệ";
+      let max = 50;
+      messageMax = `Vui lòng không nhập quá ${max} ký tự`
+      return  empty(value,messageEmpty)|| maxLength(value,max,messageMax)|| regex(value,REGEX_FULLNAME,messageRegex) ;
     }
   };
 }
@@ -89,26 +105,20 @@ Validator.isEmail = function(selector){
     selector: selector,
     test: function(value){
       const REGEX_EMAIL = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      value.trim();
-      value.toLowerCase;
-      if(value.length == 0){
-        return "Vui lòng nhập email"
-      }
-      return REGEX_EMAIL.test(value) ? undefined : "Vui lòng nhập email hợp lệ";
+      messageEmpty = `Vui lòng nhập email`;
+      messageRegex = "Vui lòng nhập email hợp lệ";
+      return empty(value,messageEmpty) || regex(value,REGEX_EMAIL,messageRegex);
     }
   };
 }
-
-Validator.isPhone = function(selector,min){
+Validator.isPhone = function(selector){
   return {
     selector: selector,
     test: function(value){
       const REGEX_PHONE = /^0\d{9}$/;
-      if(value.length == 0)
-      {
-        return "Vui lòng nhập phone"
-      }
-      return REGEX_PHONE.test(value) ? undefined : `Vui lòng nhập số điện thoại hợp lệ`;
+      messageEmpty = `Vui lòng nhập số điện thoại`;
+      messageRegex = "Vui lòng nhập số điện thoại hợp lệ";
+      return empty(value,messageEmpty) || regex(value,REGEX_PHONE,messageRegex);
     }
   };
 }
@@ -117,67 +127,41 @@ Validator.isBirthday = function(selector){
   return {
     selector: selector,
     test: function(value){
-    const REGEX_BIRTHDAY =/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
-    let parts = value.split("/");
-    let month1 = parseInt(parts[1], 10);
-    let day1 = parseInt(parts[0], 10);
-    let year1 = parseInt(parts[2], 10);
-    let date = new Date();
-    let day_now = date.getDay();
-    let month_now = date.getMonth();
-    let year_now = date.getFullYear();
-    if(year1>year_now){
-      return `Vui lòng nhập năm sinh hợp lệ`;
-    } 
-    // if(month1>month_now & year1>year_now){
-    //   return `Vui lòng nhập tháng sinh hợp lệ`;
-    // } 
-    // if(day1>day_now){
-    //   return `Vui lòng nhập ngày sinh hợp lệ`;
-    // } 
-    // if(month_now<=month){
-    //   return `Vui lòng nhập tháng hợp lệ`;
-    // } 
-    // if(day_now<=day){
-    //   return `Vui lòng nhập ngày hợp lệ`;
-    // } 
-    // console.log(day_now);
-    // console.log(month_now);
-    // console.log(year_now);
-      if(value.length == 0){
-        return "Vui lòng nhập birthday";
-      }
-      return REGEX_BIRTHDAY.test(value) ? undefined : `Vui lòng nhập birthday hợp lệ`;
+      const REGEX_BIRTHDAY =/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+      let parts = value.split("/");
+      let year1 = parseInt(parts[2], 10);
+      let date = new Date();
+      let year_now = date.getFullYear();
+      if(year1>year_now){
+        return `Vui lòng nhập năm sinh hợp lệ`;
+      } 
+      messageEmpty = "Vui lòng nhập birthday";
+      messageRegex = "Vui lòng nhập birthday hợp lệ"
+      return empty(value,messageEmpty) || regex(value,REGEX_BIRTHDAY,messageRegex);
     }
-  };
+  }
 }
 
 Validator.isPassword = function(selector,min,max){
   return {
     selector: selector,
     test: function(value){
-    const REGEX_PASSWORD=/^([a-zA-Z])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/;
+    const REGEX_PASSWORD = /^([a-zA-Z])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/;
     let parts = value.split("");
-    // console.log(parts);
     for (let i = 0; i < parts.length; i++) {
       if (!isNaN(parts[0].charAt(0))) {
        return "Vui lòng nhập ký tự đầu tiên là chữ cái"
       }
-      }
-      if(value.length == 0){
-        return "Vui lòng nhập password";
-      }
-      if(value.length < min){
-        return `Vui lòng nhập password có ít nhất ${min} ký tự`;
-      }
-      if(value.length > max){
-        return `Vui lòng nhập password có ít hơn ${max} ký tự`;
-      }
-    return REGEX_PASSWORD.test(value) ? undefined : `Vui lòng nhập mật khẩu bảo mật có chứa ký tự đặc biệt, chữ hoa, chữ thường`;
+    }
+    let min =8,max =30;
+    messageEmpty = "Vui lòng nhập mật khẩu";
+    messageRegex = "Vui lòng nhập mật khẩu bảo mật có chứa ký tự đặc biệt, chữ hoa, chữ thường";
+    messageMax = `Vui lòng nhập password không quá ${max} ký tự`;
+    messageMin = `Vui lòng nhập ít nhất ${min} ký tự`;
+    return empty(value,messageEmpty) || minLength(value,min,messageMin)|| maxLength(value,max,messageMax)|| regex(value,REGEX_PASSWORD,messageRegex);
     }
   };
 }
-
 Validator.isCfpassword = function(selector,getConfrimValue,message){
   return {
     selector: selector,
@@ -193,9 +177,10 @@ Validator.isImage = function(selector){
   return {
     selector: selector,
     test: function(value){
-      if(value.length == 0){
-        return "Vui lòng chọn avata";
-      }
+      const REGEX_IMAGE= /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i
+      messageEmpty = "Vui lòng chọn avata";
+      messageRegex = "Vui lòng lấy đúng hình ảnh";
+      return empty(value,messageEmpty) || regex(value,REGEX_IMAGE,messageRegex);
     }
   };
 }
@@ -203,11 +188,11 @@ Validator({
   form: '#form-signup',
   errorselector: '.form-message',
   rules: [
-    Validator.isRequired('#fname',50),
+    Validator.isRequired('#fname'),
     Validator.isEmail('#email'),
-    Validator.isPhone('#phone',10),
+    Validator.isPhone('#phone'),
     Validator.isBirthday('#birthday'),
-    Validator.isPassword('#password',8,30),
+    Validator.isPassword('#password'),
     Validator.isCfpassword('#cfpassword',function(){
       return document.querySelector('#form-signup #password').value;
     }, "Mật khẩu nhập lại không chính xác"),
@@ -239,42 +224,43 @@ function readFile(selectedFile,elementId){
   };
   reader.readAsDataURL(selectedFile);
 }
-let input = document.getElementById("phone");
+var input;
+input = document.getElementById("phone");
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 16) {
         event.preventDefault();
         document.getElementById("reset").click();
     }
 });
-let input = document.getElementById("fname");
+input = document.getElementById("fname");
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 16) {
         event.preventDefault();
         document.getElementById("reset").click();
     }
 });
-let input = document.getElementById("email");
+input = document.getElementById("email");
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 16) {
         event.preventDefault();
         document.getElementById("reset").click();
     }
 });
-let input = document.getElementById("password");
+input = document.getElementById("password");
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 16) {
         event.preventDefault();
         document.getElementById("reset").click();
     }
 });
-let input = document.getElementById("cfpassword");
+input = document.getElementById("cfpassword");
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 16) {
         event.preventDefault();
         document.getElementById("reset").click();
     }
 });
-let input = document.getElementById("birthday");
+input = document.getElementById("birthday");
 input.addEventListener("keyup", function(event) {
     if (event.keyCode === 16) {
         event.preventDefault();
